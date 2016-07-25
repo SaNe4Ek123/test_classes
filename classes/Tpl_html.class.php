926e2235e476d28fc06e127a17e028b3fddac7b6
Alex <SaNe4Ek2002@gmail.com>
2016-07-25 23:04:59 +0300

    commit 2. Added comment

--- classes/Tpl_html.class.php
+++ classes/Tpl_html.class.php
@@ -17,25 +17,31 @@ class Tpl_html extends Tpl{
 	protected $html_load = '';		//---   Загруженный html шаблон
 	protected $html_parse = '';		//--- Обработанный html шаблон
 	
+# Конструктор класса переопределяет рарасширение принимаемого файла на HTML и устанавливает дирректорию 
+
 	function __construct($dir = ''){
 		$this -> set_expansion('.html');	//--- Переопределяем в конструкторе расширение файла
-		return $this -> change_dir($dir);
+		return $this -> change_dir($dir); 
 	}
 //----------------------------------
+# Загрузка и обработка html шаблона
 
 	function load_html($file_name = ''){
-		# Загрузка и обработка html шаблона
 
-		$file_name = trim($file_name);
-		if(!empty($file_name) and is_string($file_name)){
+		$file_name = trim($file_name);	//--- на всякий случай убираем пробелы
+
+		if(!empty($file_name) and is_string($file_name)){  //--- Проверяем корректность переданного пргумента
+			//--- Формируем доступ к файлу
 			$dir = $this -> get_dir();
 			$exp = $this -> get_expansion();
 			$file = $dir.$file_name.$exp;
 
+			//--- Если файл существует сохраняем его в соответствующие поля.
 			if(file_exists($file)){
 				$this -> html_load = $this -> html_parse = file_get_contents($file);
 
-				if($tpl_names = $this ->get_names_tpl($this -> html_load)){
+				//--- Обрабатываем загруженный шаблон - разбиваем его на блоки соглассно фармату
+				if($tpl_names = $this ->get_names_tpl($this -> html_load)){ 
 					$this -> html_divide_by_name($tpl_names);
 					
 					return true;
@@ -46,14 +52,15 @@ class Tpl_html extends Tpl{
 	}
 //----------------------------------
 
+# Возвращает не обработанный html шаблон
 	function get_tpl_html(){
-		# Возвращает не обработанный html шаблон
 		return $this -> html_load;
 	}
 
 //-----------------------------------
+
+# Возвращает обработанный шаблон
 	function get_parse_html($comment = false){
-		# Возвращает обработанный шаблон
 		if($comment === false){
 			# Убираем все комментарии из страницы
 			$html = $this -> parse_html();
@@ -65,39 +72,53 @@ class Tpl_html extends Tpl{
 		}
 	}
 
-//-----------------------------------
-	// !!! Необходимо другое решение данной задачи.
-	private function parse_html($tpl_names = ''){
-		# Создаёт шаблон из обработанных кусочков
+//--------*** Вспомогательные методы класса (Закрытые) ***-------------
+
+# Создаёт шаблон из обработанных кусочков (создаёт web-страницу)
+	private function parse_html($comment = false, $tpl_names = ''){
 		if(empty($tpl_names)){
 			$tpl_names = $this -> get_tpl_names();
 		}
 
+		//--- Ищем и заменяем якоря на подготовленные шаблоны
 		foreach($tpl_names as $name => $tpl){
+			//---
+			if($comment === true){
+				$tpl_names[$name]['tpl'] = str_replace('{template}', $tpl['tpl'], $tpl['comment']);
+			}
+			//---
 			foreach($tpl_names as $n => $parse_tpl){
+
 				if(strstr($parse_tpl['tpl'], '{_'.$name.'_}'))
 				$tpl_names[$n]['tpl'] = str_replace('{_'.$name.'_}', $tpl['tpl'], $parse_tpl['tpl']);
 			}
 		}
+
+		//--- Заменяем якоря до тех пор пока они не закончатся
 		if(preg_match("/\{_[A-Z_\d]+_\}/", end($tpl_names)['tpl'])){
-			$this -> html_parse = $this -> parse_html($tpl_names);
+			$this -> html_parse = $this -> parse_html($comment, $tpl_names);
 		}
 		return $this -> html_parse;
 	}
 
 //------------------------------------
+
+# Возвращает массив с именами шаблонов на обрабатываемой странице
 	private function get_names_tpl($html = ''){
-		# Возвращает массив с именами шаблонов.
 		if(!empty($html) and is_string($html)){
 			$tpl_names = array();
-			$reg_tpl_raw_name = "/\<\!-- ?\>\> [A-Z_\d]+/u";
 
+			//--- Ищем по регулярному выражению комментарии с именем шаблона.
+			$reg_tpl_raw_name = "/\<\!-- ?\>\> [A-Z_\d]+/u"; 
 			if(preg_match_all($reg_tpl_raw_name, $html, $arr)){
 				foreach($arr[0] as $value){
+
+					//--- Выбираем имена из комментариев, добавляем их в массив.
 					if(preg_match("/[A-Z_\d]+/", $value, $tpl_name)){
 						$tpl_names[] = trim($tpl_name[0]);
 					}
 				}
+				//--- Возвращаем массив с именами шаблонов
 				return $tpl_names;
 			}
 		}
@@ -105,26 +126,43 @@ class Tpl_html extends Tpl{
 	}
 
 //---------------------------------------
+
+# Разделяет основной html шаблон по именам
 	private function html_divide_by_name($names = ''){
-		# Разделяет основной html шаблон по именам
 		if(is_array($names) and count($names)>0){
-			$names = array_reverse($names);
+
+			//--- Инициализируем переменные
+			$names = array_reverse($names); //--- реверсируем массив для поиска шаблонов начиная с конца страницы. (это нужно для корректной установке якорей)
 			$html = $this -> html_load;
 
 			foreach ($names as $name) {
 
+				//--- Распознаём блоки по регулярному выражению
 				$reg_tpl = "/\t*?\<\!-- \>\> ?".$name."[\w\W\s\d]+".$name." ?\<\< --\>/um";
-
-				if(preg_match($reg_tpl, $html, $result)){
+				if(preg_match($reg_tpl, $html, $tpl)){
 					
+					//--- выделяем описание шаблона
 					$reg_tpl_description = "/".$name." ?\( ?[\w\s\d\>\<]+ ?\)/u";
-					preg_match($reg_tpl_description, $result[0], $desc);
-
+					preg_match($reg_tpl_description, $tpl[0], $desc);
 					$desc = preg_replace("/[A-Z_\d_]+ ?/", '', $desc[0]);
 
-					$html = str_replace($result[0], '{_'.$name.'_}', $html);
+					//--- В основном шаблоне устанавливаем якоря вместо выделенного блока.
+					$html = str_replace($tpl[0], '{_'.$name.'_}', $html);
 
-					$this -> templates[$name]['tpl'] = $result[0];
+					//--- Отделяем шаблон от комментариев.
+					 # Ищем начальный комментарий
+					$reg_coment_1 = "/\t*?\<\!-- *?\>\> *?[A-Z_\d]+ *?\( ?[\pL \d\-_]+ *?\) *?--\>(\r\n)?/u";
+					if(preg_match($reg_coment_1, $tpl[0], $comment_1))
+						$tpl[0] = preg_replace($reg_coment_1, '', $tpl[0]);
+
+						# Ищем конечный комментарий
+					$reg_coment_2 = "/\t*?\<\!-- *?[A-Z_\d]+ ?\<\< *?--\>(\r\n)?/u";
+					if(preg_match($reg_coment_2, $tpl[0], $comment_2))
+						$tpl[0] = preg_replace($reg_coment_2, '', $tpl[0]);
+					
+					//--- Сохраняем всё в массиве
+					$this -> templates[$name]['comment'] = $comment_1[0].'{template}'.$comment_2[0];
+					$this -> templates[$name]['tpl'] = $tpl[0];
 					$this -> templates[$name]['description'] = trim($desc, '() ');
 				}
 			}
@@ -132,23 +170,7 @@ class Tpl_html extends Tpl{
 		return false;
 	}
 
+//-------------------------------------------
 } 
-	/*
-	class Tpl_from_html - класс наследник от класса Tpl
-	*** назначение - обработка цельных html шаблонов, специально отформатированных под текущий класс
-		- Цель данного класса устранить нагромождение файлов шаблонов, ограничиваясь лишь шаблонами конкретных страниц, таких как например, главная страница или страница с товарами или ещё какая другая страница. 
-		- Также ожидается, что в файловой системе станет больше порядка, а html страницы останутся цельными и читабельными, что по идее должно облегчить их редактирование (структура и т.п.)
 
-		- класс инициализирует шаблоны на лету и хранит их в массиве объекта.
-		- шаблоны могут вкладываться в шаблоны, для этого предусмотренны специальные метки.
-		- есть возможность посмотреть всю страницу на любом этапе её формирования (даже если в шаблон внесены не все данные)
-		- Каждый отдельный шаблон - это объект, хранящийся в массиве с ключём имени шаблона. T. e. редактируя шаблон, мы имеем дело с объектом свойствами которого являются ключи для вставки данных + поле 'description' - с описанием шаблона.
-
-		*** Задачи ***
-	- продумать взаимодействие шаблонов при перелинковке страниц.
-	- продумать метки для форматирования html файлов.
-	- учесть вероятность подгрузки контента без перезагрузки страницы. (возможно найдётся способ реализовать и эту функцию используя этот класс)
-
-
-	*/
 ?>
--- test.php
+++ test.php
@@ -5,7 +5,6 @@ require_once ('classes/Tpl_html.class.php');
 
 $tpl = new Tpl_html('html/');
 $a = $tpl -> load_html('test_html');
-echo $tpl -> get_parse_html(true);
-print_r($tpl -> get_tpl('FOOTER'));
 
+echo $tpl -> get_parse_html();
 ?>
